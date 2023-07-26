e5635b0a2e7539f640639ac7c007dd97a5f38eec
kisah <data.kisahtegar@gmail.com>
2023-07-26 11:42:09 +0700

    Loops : OMITTING CONDITIONS FROM A FOR LOOP IN GO

--- main.go
+++ main.go
@@ -1,4 +1,17 @@
-// Loops : LOOPS IN GO
+// Loops : OMITTING CONDITIONS FROM A FOR LOOP IN GO
+
+// ASSIGNMENT
+// Complete the maxMessages function. Given a cost threshold, it should calculate the maximum number of messages that can be sent.
+//
+// Each message costs 1.0, plus an additional fee. The fee structure is:
+//
+// 1st message: 1.0 + 0.00
+// 2nd message: 1.0 + 0.01
+// 3rd message: 1.0 + 0.02
+// 4th message: 1.0 + 0.03
+//
+// BROWSER FREEZE
+// If you lock up your browser by creating an infinite loop that isn't breaking, just click the cancel button.
 
 package main
 
@@ -6,45 +19,47 @@ import (
 	"fmt"
 )
 
-func bulkSend(numMessages int) float64 {
+func maxMessages(thresh float64) int {
 	totalCost := 0.0
-	for i := 0; i < numMessages; i++ {
-		totalCost += 1.0 + (0.01 * float64(i))
+	for i := 0; ; i++ {
+		totalCost += 1.0 + (0.1 * float64(i))
+		if totalCost > thresh {
+			return i
+		}
 	}
-	return totalCost
 }
 
 // don't edit below this line
 
-func test(numMessages int) {
-	fmt.Printf("Sending %v messages\n", numMessages)
-	cost := bulkSend(numMessages)
-	fmt.Printf("Bulk send complete! Cost = %.2f\n", cost)
+func test(thresh float64) {
+	fmt.Printf("Threshold: %.2f\n", thresh)
+	max := maxMessages(thresh)
+	fmt.Printf("Maximum messages that can be sent: = %v\n", max)
 	fmt.Println("===============================================================")
 }
 
 func main() {
-	test(10)
-	test(20)
-	test(30)
-	test(40)
-	test(50)
+	test(10.00)
+	test(20.00)
+	test(30.00)
+	test(40.00)
+	test(50.00)
 }
 
 // RESULTS:
 
-// Sending 10 messages
-// Bulk send complete! Cost = 10.45
+// Threshold: 10.00
+// Maximum messages that can be sent: = 7
 // ===============================================================
-// Sending 20 messages
-// Bulk send complete! Cost = 21.90
+// Threshold: 20.00
+// Maximum messages that can be sent: = 12
 // ===============================================================
-// Sending 30 messages
-// Bulk send complete! Cost = 34.35
+// Threshold: 30.00
+// Maximum messages that can be sent: = 16
 // ===============================================================
-// Sending 40 messages
-// Bulk send complete! Cost = 47.80
+// Threshold: 40.00
+// Maximum messages that can be sent: = 20
 // ===============================================================
-// Sending 50 messages
-// Bulk send complete! Cost = 62.25
+// Threshold: 50.00
+// Maximum messages that can be sent: = 23
 // ===============================================================
