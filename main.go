ec3e981cc6694ab33fc90889dcdf49f3e6fe7173
kisah <data.kisahtegar@gmail.com>
2023-07-26 17:46:36 +0700

    MAPS : MUTATIONS

--- main.go
+++ main.go
@@ -1,131 +1,131 @@
 /*
  */
-// MAPS : MAPS
+// MAPS : MUTATIONS
 
-// MAPS
-// Maps are similar to JavaScript objects, Python dictionaries, and Ruby hashes. Maps are a data structure that provides key->value mapping.
-
-// The zero value of a map is nil.
-
-// We can create a map by using a literal or by using the make() function:
+// MUTATIONS
 /*
-	ages := make(map[string]int)
-	ages["John"] = 37
-	ages["Mary"] = 24
-	ages["Mary"] = 21 // overwrites 24
-	ages = map[string]int{
-	"John": 37,
-	"Mary": 21,
-	}
+	// INSERT AN ELEMENT
+	m[key] = elem
+	// GET AN ELEMENT
+	elem = m[key]
+	// DELETE AN ELEMENT
+	delete(m, key)
+	// CHECK IF A KEY EXISTS
+	elem, ok := m[key]
 */
-
-// The len() function works on a map, it returns the total number of key/value pairs.
-/*
-	ages = map[string]int{
-	  "John": 37,
-	  "Mary": 21,
-	}
-	fmt.Println(len(ages)) // 2
-*/
+// If key is in m, then ok is true. If not, ok is false.
+// If key is not in the map, then elem is the zero value for the map's element type.
 
 // ASSIGNMENT
-// We can speed up our contact-info lookups by using a map! Looking up a value in a map by its key is much faster
-// than searching through a slice.
+// It's important to keep up with privacy regulations and to respect our user's data. We need a function that will delete user records.
 
-// Complete the getUserMap function. It takes a slice of names and a slice of phone numbers, and returns a map of
-// name -> user structs and potentially an error. A user struct just contains a user's name and phone number.
+// Complete the deleteIfNecessary function.
 
-// If the length of names and phoneNumbers is not equal, return an error with the string "invalid sizes".
-
-// The first name in the names slice matches the first phone number, and so on.
+// If the user doesn't exist in the map, return the error not found.
+// If they exist but aren't scheduled for deletion, return deleted as false with no errors.
+// If they exist and are scheduled for deletion, return deleted as true with no errors and delete their record from the map.
+// NOTE ON PASSING MAPS
+// Like slices, maps are also passed by reference into functions. This means that when a map is passed into a function we write,
+// we can make changes to the original, we don't have a copy.
 
 package main
 
 import (
 	"errors"
 	"fmt"
+	"sort"
 )
 
-func getUserMap(names []string, phoneNumbers []int) (map[string]user, error) {
-	userMap := make(map[string]user)
-	if len(names) != len(phoneNumbers) {
-		return nil, errors.New("invalid sizes")
+func deleteIfNecessary(users map[string]user, name string) (deleted bool, err error) {
+	existingUser, ok := users[name]
+	if !ok {
+		return false, errors.New("not found")
 	}
-	for i := 0; i < len(names); i++ {
-		name := names[i]
-		phoneNumbers := phoneNumbers[i]
-		userMap[name] = user{
-			name:        name,
-			phoneNumber: phoneNumbers,
-		}
+	if existingUser.scheduledForDeletion {
+		delete(users, name)
+		return true, nil
 	}
-	return userMap, nil
+	return false, nil
 }
 
 // don't touch below this line
 
 type user struct {
-	name        string
-	phoneNumber int
+	name                 string
+	number               int
+	scheduledForDeletion bool
 }
 
-func test(names []string, phoneNumbers []int) {
-	fmt.Println("Creating map...")
+func test(users map[string]user, name string) {
+	fmt.Printf("Attempting to delete %s...\n", name)
 	defer fmt.Println("====================================")
-	users, err := getUserMap(names, phoneNumbers)
+	deleted, err := deleteIfNecessary(users, name)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	for _, name := range names {
-		fmt.Printf("key: %v, value:\n", name)
-		fmt.Println(" - name:", users[name].name)
-		fmt.Println(" - number:", users[name].phoneNumber)
+	if deleted {
+		fmt.Println("Deleted:", name)
+		return
 	}
+	fmt.Println("Did not delete:", name)
 }
 
 func main() {
-	test(
-		[]string{"John", "Bob", "Jill"},
-		[]int{14355550987, 98765550987, 18265554567},
-	)
-	test(
-		[]string{"John", "Bob"},
-		[]int{14355550987, 98765550987, 18265554567},
-	)
-	test(
-		[]string{"George", "Sally", "Rich", "Sue"},
-		[]int{20955559812, 38385550982, 48265554567, 16045559873},
-	)
+	users := map[string]user{
+		"john": {
+			name:                 "john",
+			number:               18965554631,
+			scheduledForDeletion: true,
+		},
+		"elon": {
+			name:                 "elon",
+			number:               19875556452,
+			scheduledForDeletion: true,
+		},
+		"breanna": {
+			name:                 "breanna",
+			number:               98575554231,
+			scheduledForDeletion: false,
+		},
+		"kade": {
+			name:                 "kade",
+			number:               10765557221,
+			scheduledForDeletion: false,
+		},
+	}
+	test(users, "john")
+	test(users, "musk")
+	test(users, "santa")
+	test(users, "kade")
+
+	keys := []string{}
+	for name := range users {
+		keys = append(keys, name)
+	}
+	sort.Strings(keys)
+
+	fmt.Println("Final map keys:")
+	for _, name := range keys {
+		fmt.Println(" - ", name)
+	}
 }
 
 // RESULTS:
 
-// Creating map...
-// key: John, value:
-//  - name: John
-//  - number: 14355550987
-// key: Bob, value:
-//  - name: Bob
-//  - number: 98765550987
-// key: Jill, value:
-//  - name: Jill
-//  - number: 18265554567
+// Attempting to delete john...
+// Deleted: john
 // ====================================
-// Creating map...
-// invalid sizes
+// Attempting to delete musk...
+// not found
 // ====================================
-// Creating map...
-// key: George, value:
-//  - name: George
-//  - number: 20955559812
-// key: Sally, value:
-//  - name: Sally
-//  - number: 38385550982
-// key: Rich, value:
-//  - name: Rich
-//  - number: 48265554567
-// key: Sue, value:
-//  - name: Sue
-//  - number: 16045559873
+// Attempting to delete santa...
+// not found
 // ====================================
+// Attempting to delete kade...
+// Did not delete: kade
+// ====================================
+// Final map keys:
+//  -  breanna
+//  -  elon
+//  -  kade
