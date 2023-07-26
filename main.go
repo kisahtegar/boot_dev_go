31865fffb01964ce6cc18be3230e35379d5a44f3
kisah <data.kisahtegar@gmail.com>
2023-07-26 12:07:38 +0700

    Loops : CONTINUE (print prime number)

--- main.go
+++ main.go
@@ -1,60 +1,97 @@
-// Loops : FIZZBUZZ
+// Loops : CONTINUE
 
 // ASSIGNMENT
-// We're hiring engineers at Textio, so time to brush up on the classic "Fizzbuzz" game,
-// a coding exercise that has been dramatically overused in coding interviews across the world.
+// As an easter egg, we decided to reward our users with a free text message if they send a prime number of text messages this year.
+// Complete the printPrimes function. It should print all of the prime numbers up to and including max. It should skip any numbers that are not prime.
 
-// Complete the fizzbuzz function that prints the numbers 1 to 100 inclusive each on their own line,
-// but substitutes multiples of 3 for the text fizz and multiples of 5 for buzz. For multiples of 3 AND 5 print instead fizzbuzz.
+// Here's the psuedocode:
+//
+// printPrimes(max):
+//   for n in range(2, max+1):
+//     if n is 2:
+//       n is prime, print it
+//     if n is even:
+//       n is not prime, skip to next n
+//     for i in range (3, sqrt(n) + 1):
+//       if i can be multiplied into n:
+//         n is not prime, skip to next n
+//     n is prime, print it
+
+// BREAKDOWN
+// We skip even numbers because they can't be prime
+// We only check up to the square root because anything higher than the square root has no chance of multiplying evenly into n
+// We start checking at 2 because 1 is not prime
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func fizzbuzz() {
-	for i := 1; i < 100; i++ {
-		if i%3 == 0 && i%5 == 0 {
-			fmt.Println("fizzbuzz")
-		} else if i%3 == 0 {
-			fmt.Println("fizz")
-		} else if i%5 == 0 {
-			fmt.Println("buzz")
-		} else {
-			fmt.Println(i)
+func printPrimes(max int) {
+	for n := 2; n < max+1; n++ {
+		if n == 2 {
+			fmt.Println(n)
+			continue
 		}
+		if n%2 == 0 {
+			continue
+		}
+		isPrime := true
+		for i := 3; i*i < n+i; i++ {
+			if n%i == 0 {
+				isPrime = false
+				break
+			}
+		}
+		if !isPrime {
+			continue
+		}
+		fmt.Println(n)
 	}
 }
 
-// don't touch below this line
+// don't edit below this line
+
+func test(max int) {
+	fmt.Printf("Primes up to %v:\n", max)
+	printPrimes(max)
+	fmt.Println("===============================================================")
+}
 
 func main() {
-	fizzbuzz()
+	test(10)
+	test(20)
+	test(30)
 }
 
 // RESULTS:
 
-// 1
+// Primes up to 10:
 // 2
-// fizz
-// 4
-// buzz
-// fizz
+// 3
+// 5
 // 7
-// 8
-// fizz
-// buzz
+// ===============================================================
+// Primes up to 20:
+// 2
+// 3
+// 5
+// 7
 // 11
-// fizz
 // 13
-// 14
-// fizzbuzz
-// 16
 // 17
-// fizz
 // 19
-// buzz
-// fizz
-// 22
+// ===============================================================
+// Primes up to 30:
+// 2
+// 3
+// 5
+// 7
+// 11
+// 13
+// 17
+// 19
 // 23
-// fizz
-// ....
+// 29
+// ===============================================================
