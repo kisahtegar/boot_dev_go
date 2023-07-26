5faf8cae8793365301b5421f923f3d397135e45f
kisah <data.kisahtegar@gmail.com>
2023-07-26 20:33:32 +0700

    Advanced Functions : CURRYING

--- main.go
+++ main.go
@@ -1,30 +1,109 @@
 /*
  */
-// Advanced Functions : WHY FIRST-CLASS AND HIGHER-ORDER FUNCTIONS?
+// Advanced Functions : CURRYING
 
-// WHY FIRST-CLASS AND HIGHER-ORDER FUNCTIONS?
-// At first, it may seem like dynamically creating functions and passing them around as variables adds
-// unnecessary complexity. Most of the time you would be right. There are cases however when functions
-// as values make a lot of sense. Some of these include:
+// CURRYING
+// Function currying is the practice of writing a function that takes a function (or functions) as input,
+// and returns a new function.
 
-// - HTTP API handlers
-// - Pub/Sub handlers
-// - Onclick callbacks
-
-// Any time you need to run custom code at a time in the future, functions as values might make sense.
-
-// DEFINITION: FIRST-CLASS FUNCTIONS
-// A first-class function is a function that can be treated like any other value. Go supports first-class
-// functions. A function's type is dependent on the types of its parameters and return values. For example,
-// these are different function types:
+// For example:
 /*
-	func() int
-	func(string) int
-*/
+	func main() {
+		squareFunc := selfMath(multiply)
+		doubleFunc := selfMath(add)
 
-// DEFINITION: HIGHER-ORDER FUNCTIONS
-// A higher-order function is a function that takes a function as an argument or returns a function as a
-// return value. Go supports higher-order functions. For example, this function takes a function as an argument:
-/*
-	func aggregate(a, b, c int, arithmetic func(int, int) int) int
+		fmt.Println(squareFunc(5))
+		// prints 25
+
+		fmt.Println(doubleFunc(5))
+		// prints 10
+	}
+
+	func multiply(x, y int) int {
+		return x * y
+	}
+
+	func add(x, y int) int {
+		return x + y
+	}
+
+	func selfMath(mathFunc func(int, int) int) func (int) int {
+		return func(x int) int {
+			return mathFunc(x, x)
+		}
+	}
 */
+// In the example above, the selfMath function takes in a function as its parameter, and returns a function
+// that itself returns the value of running that input function on its parameter.
+
+// ASSIGNMENT
+// The Mailio API needs a very robust error-logging system so we can see when things are going awry in the
+// back-end system. We need a function that can create a custom "logger" (a function that prints to the console)
+// given a specific formatter.
+
+// Complete the getLogger function. It should return a new function that prints the formatted inputs using the
+// given formatter function. The inputs should be passed into the formatter function in the order they are given
+// to the logger function.
+
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+// getLogger takes a function that formats two strings into
+// a single string and returns a function that formats two strings but prints
+// the result instead of returning it
+func getLogger(formatter func(string, string) string) func(string, string) {
+	return func(a string, b string) {
+		fmt.Println(formatter(a, b))
+	}
+}
+
+// don't touch below this line
+
+func test(first string, errors []error, formatter func(string, string) string) {
+	defer fmt.Println("====================================")
+	logger := getLogger(formatter)
+	fmt.Println("Logs:")
+	for _, err := range errors {
+		logger(first, err.Error())
+	}
+}
+
+func colonDelimit(first, second string) string {
+	return first + ": " + second
+}
+func commaDelimit(first, second string) string {
+	return first + ", " + second
+}
+
+func main() {
+	dbErrors := []error{
+		errors.New("out of memory"),
+		errors.New("cpu is pegged"),
+		errors.New("networking issue"),
+		errors.New("invalid syntax"),
+	}
+	test("Error on database server", dbErrors, colonDelimit)
+
+	mailErrors := []error{
+		errors.New("email too large"),
+		errors.New("non alphanumeric symbols found"),
+	}
+	test("Error on mail server", mailErrors, commaDelimit)
+}
+
+// RESULTS:
+
+// Logs:
+// Error on database server: out of memory
+// Error on database server: cpu is pegged
+// Error on database server: networking issue
+// Error on database server: invalid syntax
+// ====================================
+// Logs:
+// Error on mail server, email too large
+// Error on mail server, non alphanumeric symbols found
+// ====================================
