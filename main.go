1504600d8a934d6bc3744ff5ed5ed9d6310ac0b7
kisah <data.kisahtegar@gmail.com>
2023-07-26 21:11:30 +0700

    Pointer : INTRODUCTION TO POINTERS

--- main.go
+++ main.go
@@ -1,97 +1,79 @@
 /*
  */
-// Advanced Functions : ANONYMOUS FUNCTIONS
+// Pointer : INTRODUCTION TO POINTERS
 
-// ANONYMOUS FUNCTIONS
-// Anonymous functions are true to form in that they have no name. We've been using them
-// throughout this chapter, but we haven't really talked about them yet.
-
-// Anonymous functions are useful when defining a function that will only be used once
-// or to create a quick closure.
+// INTRODUCTION TO POINTERS
+// As we have learned, a variable is a named location in memory that stores a value.
+// We can manipulate the value of a variable by assigning a new value to it or by
+// performing operations on it. When we assign a value to a variable, we are storing
+// that value in a specific location in memory.
 /*
-	// doMath accepts a function that converts one int into another
-	// and a slice of ints. It returns a slice of ints that have been
-	// converted by the passed in function.
-	func doMath(f func(int) int, nums []int) []int {
-		var results []int
-		for _, n := range nums {
-			results = append(results, f(n))
-		}
-		return results
-	}
-
-	func main() {
-		nums := []int{1, 2, 3, 4, 5}
-
-		// Here we define an anonymous function that doubles an int
-		// and pass it to doMath
-		allNumsDoubled := doMath(func(x int) int {
-			return x + x
-		}, nums)
-
-		fmt.Println(allNumsDoubled)
-		// prints:
-		// [2 4 6 8 10]
-	}
+	x := 42
+	// "x" is the name of a location in memory. That location is storing the integer value of 42
 */
 
-// ASSIGNMENT
-// Complete the printReports function.
+// A POINTER IS A VARIABLE
+// A pointer is a variable that stores the memory address of another variable. This
+// means that a pointer "points to" the location of where the data is stored NOT the
+// actual data itself.
 
-// Call printCostReport once for each message. Pass in an anonymous function as the
-// costCalculator that returns an int equal to twice the length of the input message.
-// within a closure.
+/*
+	// The * syntax defines a pointer:
+	var p *int
+
+	// The & operator generates a pointer to its operand.
+	myString := "hello"
+	myStringPtr = &myString
+*/
+
+// WHY ARE POINTERS USEFUL?
+// Pointers allow us to manipulate data in memory directly, without making copies or
+// duplicating data. This can make programs more efficient and allow us to do things
+// that would be difficult or impossible without them.
+
+// ASSIGNMENT
+// Fix the bug in the sendMessage function. It's supposed to print a nicely formatted
+// message to the console containing an SMS's recipient and message body. However,
+// it's not working as expected. Run the code and see what happens, then fix the bug.
 
 package main
 
 import "fmt"
 
-func printReports(messages []string) {
-	for _, message := range messages {
-		printCostReport(func(msg string) int {
-			return len(msg) * 2
-		}, message)
-	}
+type Message struct {
+	Recipient string
+	Text      string
 }
 
-// don't touch below this line
+// Don't touch above this line
 
-func test(messages []string) {
-	defer fmt.Println("====================================")
-	printReports(messages)
+func sendMessage(m Message) {
+	fmt.Printf("To: %v\n", m.Recipient)
+	fmt.Printf("Message: %v\n", m.Text)
+}
+
+// Don't touch below this line
+
+func test(recipient string, text string) {
+	m := Message{Recipient: recipient, Text: text}
+	sendMessage(m)
+	fmt.Println("=====================================")
 }
 
 func main() {
-	test([]string{
-		"Here's Johnny!",
-		"Go ahead, make my day",
-		"You had me at hello",
-		"There's no place like home",
-	})
-
-	test([]string{
-		"Hello, my name is Inigo Montoya. You killed my father. Prepare to die.",
-		"May the Force be with you.",
-		"Show me the money!",
-		"Go ahead, make my day.",
-	})
-}
-
-func printCostReport(costCalculator func(string) int, message string) {
-	cost := costCalculator(message)
-	fmt.Printf(`Message: "%s" Cost: %v cents`, message, cost)
-	fmt.Println()
+	test("Lane", "Textio is getting better everyday!")
+	test("Allan", "This pointer stuff is weird...")
+	test("Tiffany", "What time will you be home for dinner?")
 }
 
 // RESULTS:
 
-// Message: "Here's Johnny!" Cost: 28 cents
-// Message: "Go ahead, make my day" Cost: 42 cents
-// Message: "You had me at hello" Cost: 38 cents
-// Message: "There's no place like home" Cost: 52 cents
-// ====================================
-// Message: "Hello, my name is Inigo Montoya. You killed my father. Prepare to die." Cost: 140 cents
-// Message: "May the Force be with you." Cost: 52 cents
-// Message: "Show me the money!" Cost: 36 cents
-// Message: "Go ahead, make my day." Cost: 44 cents
-// ====================================
+// To: Lane
+// Message: Textio is getting better everyday!
+// =====================================
+// To: Allan
+// Message: This pointer stuff is weird...
+// =====================================
+// To: Tiffany
+// Message: What time will you be home for dinner?
+// =====================================
