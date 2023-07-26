a68e7c6a49c885d6d3b27b9c56f878ca6c2c34a9
kisah <data.kisahtegar@gmail.com>
2023-07-26 20:53:07 +0700

    Advanced Functions : CLOSURES

--- main.go
+++ main.go
@@ -1,166 +1,114 @@
 /*
  */
-// Advanced Functions : DEFER
+// Advanced Functions : CLOSURES
 
-// DEFER
-// The defer keyword is a fairly unique feature of Go. It allows a function to be executed automatically
-// just before its enclosing function returns.
+// CLOSURES
+// A closure is a function that references variables from outside its own function body.
+// The function may access and assign to the referenced variables.
 
-// The deferred call's arguments are evaluated immediately, but the function call is not executed until
-// the surrounding function returns.
-
-// Deferred functions are typically used to close database connections, file handlers and the like.
-
-// For example:
+// In this example, the concatter() function returns a function that has reference to an
+// enclosed doc value. Each successive call to harryPotterAggregator mutates that same
+// doc variable.
 /*
-	// CopyFile copies a file from srcName to dstName on the local filesystem.
-	func CopyFile(dstName, srcName string) (written int64, err error) {
-
-		// Open the source file
-		src, err := os.Open(srcName)
-		if err != nil {
-			return
+	func concatter() func(string) string {
+		doc := ""
+		return func(word string) string {
+			doc += word + " "
+			return doc
 		}
-		// Close the source file when the CopyFile function returns
-		defer src.Close()
+	}
+	func main() {
+		harryPotterAggregator := concatter()
+		harryPotterAggregator("Mr.")
+		harryPotterAggregator("and")
+		harryPotterAggregator("Mrs.")
+		harryPotterAggregator("Dursley")
+		harryPotterAggregator("of")
+		harryPotterAggregator("number")
+		harryPotterAggregator("four,")
+		harryPotterAggregator("Privet")
 
-		// Create the destination file
-		dst, err := os.Create(dstName)
-		if err != nil {
-			return
-		}
-		// Close the destination file when the CopyFile function returns
-		defer dst.Close()
-
-		return io.Copy(dst, src)
+		fmt.Println(harryPotterAggregator("Drive"))
+		// Mr. and Mrs. Dursley of number four, Privet Drive
 	}
 */
-// In the above example, the src.Close() function is not called until after the CopyFile function was
-// called but immediately before the CopyFile function returns.
-
-// Defer is a great way to make sure that something happens at the end of a function, even if there
-// are multiple return statements.
 
 // ASSIGNMENT
-// There is a bug in the logAndDelete function, fix it!
+// Keeping track of how many emails we send is mission-critical at Mailio. Complete the
+// adder() function.
 
-// This function should always delete the user from the user's map, which is a map that stores the
-// user's name as keys. It also returns a log string that indicates to the caller some information
-// about the user's deletion.
-
-// To avoid bugs like this in the future, instead of calling delete before each return, just defer
-// the delete once at the beginning of the function.
+// It should return a function that adds its input (an int) to an enclosed sum value,
+// then return the new sum. In other words, it keeps a running total of the sum variable
+// within a closure.
 
 package main
 
-import (
-	"fmt"
-	"sort"
-)
+import "fmt"
 
-const (
-	logDeleted  = "user deleted"
-	logNotFound = "user not found"
-	logAdmin    = "admin deleted"
-)
-
-func logAndDelete(users map[string]user, name string) (log string) {
-	defer delete(users, name)
-	user, ok := users[name]
-	if !ok {
-		// delete(users, name)
-		return logNotFound
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
 	}
-	if user.admin {
-		return logAdmin
-	}
-	// delete(users, name)
-	return logDeleted
 }
 
 // don't touch below this line
 
-type user struct {
-	name   string
-	number int
-	admin  bool
+type emailBill struct {
+	costInPennies int
 }
 
-func test(users map[string]user, name string) {
-	fmt.Printf("Attempting to delete %s...\n", name)
+func test(bills []emailBill) {
 	defer fmt.Println("====================================")
-	log := logAndDelete(users, name)
-	fmt.Println("Log:", log)
+	countAdder, costAdder := adder(), adder()
+	for _, bill := range bills {
+		fmt.Println(bill)
+		fmt.Printf("You've sent %d emails and it has cost you %d cents\n", countAdder(1), costAdder(bill.costInPennies))
+	}
 }
 
 func main() {
-	users := map[string]user{
-		"john": {
-			name:   "john",
-			number: 18965554631,
-			admin:  true,
-		},
-		"elon": {
-			name:   "elon",
-			number: 19875556452,
-			admin:  true,
-		},
-		"breanna": {
-			name:   "breanna",
-			number: 98575554231,
-			admin:  false,
-		},
-		"kade": {
-			name:   "kade",
-			number: 10765557221,
-			admin:  false,
-		},
-	}
+	test([]emailBill{
+		{45},
+		{32},
+		{43},
+		{12},
+		{34},
+		{54},
+	})
 
-	fmt.Println("Initial users:")
-	usersSorted := []string{}
-	for name := range users {
-		usersSorted = append(usersSorted, name)
-	}
-	sort.Strings(usersSorted)
-	for _, name := range usersSorted {
-		fmt.Println(" -", name)
-	}
-	fmt.Println("====================================")
+	test([]emailBill{
+		{12},
+		{12},
+		{976},
+		{12},
+		{543},
+	})
 
-	test(users, "john")
-	test(users, "santa")
-	test(users, "kade")
-
-	fmt.Println("Final users:")
-	usersSorted = []string{}
-	for name := range users {
-		usersSorted = append(usersSorted, name)
-	}
-	sort.Strings(usersSorted)
-	for _, name := range usersSorted {
-		fmt.Println(" -", name)
-	}
-	fmt.Println("====================================")
+	test([]emailBill{
+		{743},
+		{13},
+		{8},
+	})
 }
 
 // RESULTS:
 
-// Initial users:
-//  - breanna
-//  - elon
-//  - john
-//  - kade
+// You've sent 1 emails and it has cost you 45 cents
+// You've sent 2 emails and it has cost you 77 cents
+// You've sent 3 emails and it has cost you 120 cents
+// You've sent 4 emails and it has cost you 132 cents
+// You've sent 5 emails and it has cost you 166 cents
+// You've sent 6 emails and it has cost you 220 cents
 // ====================================
-// Attempting to delete john...
-// Log: admin deleted
+// You've sent 1 emails and it has cost you 12 cents
+// You've sent 2 emails and it has cost you 24 cents
+// You've sent 3 emails and it has cost you 1000 cents
+// You've sent 4 emails and it has cost you 1012 cents
+// You've sent 5 emails and it has cost you 1555 cents
 // ====================================
-// Attempting to delete santa...
-// Log: user not found
+// You've sent 1 emails and it has cost you 743 cents
+// You've sent 2 emails and it has cost you 756 cents
+// You've sent 3 emails and it has cost you 764 cents
 // ====================================
-// Attempting to delete kade...
-// Log: user deleted
-// ====================================
-// Final users:
-//  - breanna
-//  - elon
