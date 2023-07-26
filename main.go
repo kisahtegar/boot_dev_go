05c90aa89b13f1c11da19d565fda49d3f754692e
kisah <data.kisahtegar@gmail.com>
2023-07-26 21:39:15 +0700

    Pointer : POINTER RECEIVER CODE

--- main.go
+++ main.go
@@ -1,50 +1,112 @@
 /*
  */
-// Pointer : POINTER RECEIVERS
+// Pointer : POINTER RECEIVER CODE
 
-// POINTER RECEIVERS
-// A receiver type on a method can be a pointer.
-
-// Methods with pointer receivers can modify the value to which the receiver points.
-// Since methods often need to modify their receiver, pointer receivers are more
-// common than value receivers.
-
-// POINTER RECEIVER
+// POINTER RECEIVER CODE
+// Methods with pointer receivers don't require that a pointer is used to call the
+// method. The pointer will automatically be derived from the value.
 /*
-	type car struct {
-		color string
+	type circle struct {
+		x int
+		y int
+		radius int
 	}
 
-	func (c *car) setColor(color string) {
-		c.color = color
+	func (c *circle) grow(){
+		c.radius *= 2
 	}
 
-	func main() {
-		c := car{
-			color: "white",
+	func main(){
+		c := circle{
+			x: 1,
+			y: 2,
+			radius: 4,
 		}
-		c.setColor("blue")
-		fmt.Println(c.color)
-		// prints "blue"
+
+		// notice c is not a pointer in the calling function
+		// but the method still gains access to a pointer to c
+		c.grow()
+		fmt.Println(c.radius)
+		// prints 8
 	}
 */
 
-// NON-POINTER RECEIVER
-/*
-	type car struct {
-		color string
-	}
+// ASSIGNMENT
+// Fix the bug in the code so that setMessage sets the message field of the given
+// email structure, and the new value persists outside the scope of the setMessage
+// method.
 
-	func (c car) setColor(color string) {
-		c.color = color
-	}
+package main
 
-	func main() {
-		c := car{
-			color: "white",
-		}
-		c.setColor("blue")
-		fmt.Println(c.color)
-		// prints "white"
-	}
-*/
+import (
+	"fmt"
+)
+
+func (e *email) setMessage(newMessage string) {
+	e.message = newMessage
+}
+
+// don't edit below this line
+
+type email struct {
+	message     string
+	fromAddress string
+	toAddress   string
+}
+
+func test(e *email, newMessage string) {
+	fmt.Println("-- before --")
+	e.print()
+	fmt.Println("-- end before --")
+	e.setMessage("this is my second draft")
+	fmt.Println("-- after --")
+	e.print()
+	fmt.Println("-- end after --")
+	fmt.Println("==========================")
+}
+
+func (e email) print() {
+	fmt.Println("message:", e.message)
+	fmt.Println("fromAddress:", e.fromAddress)
+	fmt.Println("toAddress:", e.toAddress)
+}
+
+func main() {
+	test(&email{
+		message:     "this is my first draft",
+		fromAddress: "sandra@mailio-test.com",
+		toAddress:   "bullock@mailio-test.com",
+	}, "this is my second draft")
+
+	test(&email{
+		message:     "this is my third draft",
+		fromAddress: "sandra@mailio-test.com",
+		toAddress:   "bullock@mailio-test.com",
+	}, "this is my fourth draft")
+
+}
+
+// RESULTS:
+
+// -- before --
+// message: this is my first draft
+// fromAddress: sandra@mailio-test.com
+// toAddress: bullock@mailio-test.com
+// -- end before --
+// -- after --
+// message: this is my second draft
+// fromAddress: sandra@mailio-test.com
+// toAddress: bullock@mailio-test.com
+// -- end after --
+// ==========================
+// -- before --
+// message: this is my third draft
+// fromAddress: sandra@mailio-test.com
+// toAddress: bullock@mailio-test.com
+// -- end before --
+// -- after --
+// message: this is my second draft
+// fromAddress: sandra@mailio-test.com
+// toAddress: bullock@mailio-test.com
+// -- end after --
+// ==========================
