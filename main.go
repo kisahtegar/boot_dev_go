71f42e0b8e33b034382535ed22dc2574d4c6f2a7
kisah <data.kisahtegar@gmail.com>
2023-07-26 19:16:01 +0700

    Advanced Functions : WHY FIRST-CLASS AND HIGHER-ORDER FUNCTIONS?

--- main.go
+++ main.go
@@ -1,104 +1,30 @@
 /*
  */
-// Advanced Functions : FIRST CLASS AND HIGHER ORDER FUNCTIONS
+// Advanced Functions : WHY FIRST-CLASS AND HIGHER-ORDER FUNCTIONS?
 
-// FIRST CLASS AND HIGHER ORDER FUNCTIONS
-// A programming language is said to have "first-class functions" when functions in that language are treated like any
-// other variable. For example, in such a language, a function can be passed as an argument to other functions, can be
-// returned by another function and can be assigned as a value to a variable.
+// WHY FIRST-CLASS AND HIGHER-ORDER FUNCTIONS?
+// At first, it may seem like dynamically creating functions and passing them around as variables adds
+// unnecessary complexity. Most of the time you would be right. There are cases however when functions
+// as values make a lot of sense. Some of these include:
 
-// A function that returns a function or accepts a function as input is called a Higher-Order Function.
+// - HTTP API handlers
+// - Pub/Sub handlers
+// - Onclick callbacks
 
-// Go supports first-class and higher-order functions. Another way to think of this is that a function is just another
-// type -- just like ints and strings and bools.
+// Any time you need to run custom code at a time in the future, functions as values might make sense.
 
-// For example, to accept a function as a parameter:
+// DEFINITION: FIRST-CLASS FUNCTIONS
+// A first-class function is a function that can be treated like any other value. Go supports first-class
+// functions. A function's type is dependent on the types of its parameters and return values. For example,
+// these are different function types:
 /*
-	func add(x, y int) int {
-		return x + y
-	}
-
-	func mul(x, y int) int {
-		return x * y
-	}
-
-	// aggregate applies the given math function to the first 3 inputs
-	func aggregate(a, b, c int, arithmetic func(int, int) int) int {
-		return arithmetic(arithmetic(a, b), c)
-	}
-
-	func main(){
-		fmt.Println(aggregate(2,3,4, add))
-		// prints 9
-		fmt.Println(aggregate(2,3,4, mul))
-		// prints 24
-	}
+	func() int
+	func(string) int
 */
 
-// ASSIGNMENT
-// Textio is launching a new email messaging product, "Mailio"!
-
-// Fix the compile-time bug in the getFormattedMessages function. The function body is correct, but the function signature is not.
-
-package main
-
-import "fmt"
-
-func getFormattedMessages(messages []string, formatter func(string) string) []string {
-	formattedMessages := []string{}
-	for _, message := range messages {
-		formattedMessages = append(formattedMessages, formatter(message))
-	}
-	return formattedMessages
-}
-
-// don't touch below this line
-
-func addSignature(message string) string {
-	return message + " Kind regards."
-}
-
-func addGreeting(message string) string {
-	return "Hello! " + message
-}
-
-func test(messages []string, formatter func(string) string) {
-	defer fmt.Println("====================================")
-	formattedMessages := getFormattedMessages(messages, formatter)
-	if len(formattedMessages) != len(messages) {
-		fmt.Println("The number of messages returned is incorrect.")
-		return
-	}
-	for i, message := range messages {
-		formatted := formattedMessages[i]
-		fmt.Printf(" * %s -> %s\n", message, formatted)
-	}
-}
-
-func main() {
-	test([]string{
-		"Thanks for getting back to me.",
-		"Great to see you again.",
-		"I would love to hang out this weekend.",
-		"Got any hot stock tips?",
-	}, addSignature)
-	test([]string{
-		"Thanks for getting back to me.",
-		"Great to see you again.",
-		"I would love to hang out this weekend.",
-		"Got any hot stock tips?",
-	}, addGreeting)
-}
-
-// RESULTS:
-
-// * Thanks for getting back to me. -> Thanks for getting back to me. Kind regards.
-// * Great to see you again. -> Great to see you again. Kind regards.
-// * I would love to hang out this weekend. -> I would love to hang out this weekend. Kind regards.
-// * Got any hot stock tips? -> Got any hot stock tips? Kind regards.
-// ====================================
-// * Thanks for getting back to me. -> Hello! Thanks for getting back to me.
-// * Great to see you again. -> Hello! Great to see you again.
-// * I would love to hang out this weekend. -> Hello! I would love to hang out this weekend.
-// * Got any hot stock tips? -> Hello! Got any hot stock tips?
-// ====================================
+// DEFINITION: HIGHER-ORDER FUNCTIONS
+// A higher-order function is a function that takes a function as an argument or returns a function as a
+// return value. Go supports higher-order functions. For example, this function takes a function as an argument:
+/*
+	func aggregate(a, b, c int, arithmetic func(int, int) int) int
+*/
