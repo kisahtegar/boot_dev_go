933f4b1aa1b9d636aee8036dd063051347d4596b
kisah <data.kisahtegar@gmail.com>
2023-07-26 16:27:32 +0700

    Slices : SLICE OF SLICES

--- main.go
+++ main.go
@@ -1,154 +1,106 @@
 /*
  */
-// Slices : APPEND
+// Slices : SLICE OF SLICES
 
-// APPEND
-// The built-in append function is used to dynamically add elements to a slice:
-
+// SLICE OF SLICES
+// Slices can hold other slices, effectively creating a matrix, or a 2D slice.
 /*
-	func append(slice []Type, elems ...Type) []Type
-	If the underlying array is not large enough, append() will create a new underlying array and point the slice to it.
-*/
-
-// Notice that append() is variadic, the following are all valid:
-/*
-	slice = append(slice, oneThing)
-	slice = append(slice, firstThing, secondThing)
-	slice = append(slice, anotherSlice...)
+	rows := [][]int{}
 */
 
 // ASSIGNMENT
-// We've been asked to "bucket" costs for an entire month into the cost occurring on each day of the month.
+// We support various graphs and dashboards on Textio that display message analytics to our users.
+// The UI for our graphs and charts is built on top of a grid system. Let's build some grid logic.
 
-// Complete the getCostsByDay function. It should return a slice of float64s, where each element is the total cost
-// for that day. The length of the slice should be equal to the number of days represented in the costs slice,
-// including any days that have no costs, up to the last day represented in the slice.
+// Complete the createMatrix function. It takes a number of rows and columns and returns a 2D slice
+// of integers where the value of each cell is i * j where i and j are the indexes of the row and column respectively.
 
-// Days are numbered and start at 0.
-
-// EXAMPLE
-// Input in day, cost format:
-/*
-	[]cost{
-		{0, 4.0},
-		{1, 2.1},
-		{1, 3.1},
-		{5, 2.5},
-	}
-*/
-// I would expect this result:
-/*
-	[]float64{
-		4.0,
-		5.2,
-		0.0,
-		0.0,
-		0.0,
-		2.5,
-	}
-*/
+// For example, a 10x10 matrix would look like this:
+// [0 0 0 0 0 0 0 0 0 0]
+// [0 1 2 3 4 5 6 7 8 9]
+// [0 2 4 6 8 10 12 14 16 18]
+// [0 3 6 9 12 15 18 21 24 27]
+// [0 4 8 12 16 20 24 28 32 36]
+// [0 5 10 15 20 25 30 35 40 45]
+// [0 6 12 18 24 30 36 42 48 54]
+// [0 7 14 21 28 35 42 49 56 63]
+// [0 8 16 24 32 40 48 56 64 72]
+// [0 9 18 27 36 45 54 63 72 81]
 
 package main
 
 import "fmt"
 
-type cost struct {
-	day   int
-	value float64
-}
-
-func getCostsByDay(costs []cost) []float64 {
-	costsByDay := []float64{}
-	for i := 0; i < len(costs); i++ {
-		cost := costs[i]
-		for cost.day >= len(costsByDay) {
-			costsByDay = append(costsByDay, 0.0)
-			// fmt.Println(costsByDay)
+func createMatrix(rows, cols int) [][]int {
+	matrix := make([][]int, 0)
+	for i := 0; i < rows; i++ {
+		row := make([]int, 0)
+		for j := 0; j < cols; j++ {
+			row = append(row, i*j)
 		}
-		costsByDay[cost.day] += cost.value
-		fmt.Println(costsByDay)
+		matrix = append(matrix, row)
 	}
-	return costsByDay
+	return matrix
 }
 
 // dont edit below this line
 
-func test(costs []cost) {
-	fmt.Printf("Creating daily buckets for %v costs...\n", len(costs))
-	costsByDay := getCostsByDay(costs)
-	fmt.Println("Costs by day:")
-	for i := 0; i < len(costsByDay); i++ {
-		fmt.Printf(" - Day %v: %.2f\n", i, costsByDay[i])
+func test(rows, cols int) {
+	fmt.Printf("Creating %v x %v matrix...\n", rows, cols)
+	matrix := createMatrix(rows, cols)
+	for i := 0; i < len(matrix); i++ {
+		fmt.Println(matrix[i])
 	}
 	fmt.Println("===== END REPORT =====")
 }
 
 func main() {
-	test([]cost{
-		{0, 1.0},
-		{1, 2.0},
-		{1, 3.1},
-		{2, 2.5},
-		{3, 3.6},
-		{3, 2.7},
-		{4, 3.34},
-	})
-	test([]cost{
-		{0, 1.0},
-		{10, 2.0},
-		{3, 3.1},
-		{2, 2.5},
-		{1, 3.6},
-		{2, 2.7},
-		{4, 56.34},
-		{13, 2.34},
-		{28, 1.34},
-		{25, 2.34},
-		{30, 4.34},
-	})
+	test(3, 3)
+	test(5, 5)
+	test(10, 10)
+	test(15, 15)
 }
 
 // RESULTS:
 
-// Creating daily buckets for 7 costs...
-// Costs by day:
-//  - Day 0: 1.00
-//  - Day 1: 5.10
-//  - Day 2: 2.50
-//  - Day 3: 6.30
-//  - Day 4: 3.34
+// Creating 3 x 3 matrix...
+// [0 0 0]
+// [0 1 2]
+// [0 2 4]
 // ===== END REPORT =====
-// Creating daily buckets for 11 costs...
-// Costs by day:
-//  - Day 0: 1.00
-//  - Day 1: 3.60
-//  - Day 2: 5.20
-//  - Day 3: 3.10
-//  - Day 4: 56.34
-//  - Day 5: 0.00
-//  - Day 6: 0.00
-//  - Day 7: 0.00
-//  - Day 8: 0.00
-//  - Day 9: 0.00
-//  - Day 10: 2.00
-//  - Day 11: 0.00
-//  - Day 12: 0.00
-//  - Day 13: 2.34
-//  - Day 14: 0.00
-//  - Day 15: 0.00
-//  - Day 16: 0.00
-//  - Day 17: 0.00
-//  - Day 18: 0.00
-//  - Day 19: 0.00
-//  - Day 20: 0.00
-//  - Day 21: 0.00
-//  - Day 22: 0.00
-//  - Day 23: 0.00
-//  - Day 24: 0.00
-//  - Day 25: 2.34
-//  - Day 26: 0.00
-//  - Day 27: 0.00
-//  - Day 28: 1.34
-//  - Day 29: 0.00
-//  - Day 30: 4.34
+// Creating 5 x 5 matrix...
+// [0 0 0 0 0]
+// [0 1 2 3 4]
+// [0 2 4 6 8]
+// [0 3 6 9 12]
+// [0 4 8 12 16]
+// ===== END REPORT =====
+// Creating 10 x 10 matrix...
+// [0 0 0 0 0 0 0 0 0 0]
+// [0 1 2 3 4 5 6 7 8 9]
+// [0 2 4 6 8 10 12 14 16 18]
+// [0 3 6 9 12 15 18 21 24 27]
+// [0 4 8 12 16 20 24 28 32 36]
+// [0 5 10 15 20 25 30 35 40 45]
+// [0 6 12 18 24 30 36 42 48 54]
+// [0 7 14 21 28 35 42 49 56 63]
+// [0 8 16 24 32 40 48 56 64 72]
+// [0 9 18 27 36 45 54 63 72 81]
+// ===== END REPORT =====
+// Creating 15 x 15 matrix...
+// [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+// [0 1 2 3 4 5 6 7 8 9 10 11 12 13 14]
+// [0 2 4 6 8 10 12 14 16 18 20 22 24 26 28]
+// [0 3 6 9 12 15 18 21 24 27 30 33 36 39 42]
+// [0 4 8 12 16 20 24 28 32 36 40 44 48 52 56]
+// [0 5 10 15 20 25 30 35 40 45 50 55 60 65 70]
+// [0 6 12 18 24 30 36 42 48 54 60 66 72 78 84]
+// [0 7 14 21 28 35 42 49 56 63 70 77 84 91 98]
+// [0 8 16 24 32 40 48 56 64 72 80 88 96 104 112]
+// [0 9 18 27 36 45 54 63 72 81 90 99 108 117 126]
+// [0 10 20 30 40 50 60 70 80 90 100 110 120 130 140]
+// [0 11 22 33 44 55 66 77 88 99 110 121 132 143 154]
+// [0 12 24 36 48 60 72 84 96 108 120 132 144 156 168]
+// [0 13 26 39 52 65 78 91 104 117 130 143 156 169 182]
+// [0 14 28 42 56 70 84 98 112 126 140 154 168 182 196]
 // ===== END REPORT =====
