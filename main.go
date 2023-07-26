a1681a07a381e21935433c5655d6e08e4736799b
kisah <data.kisahtegar@gmail.com>
2023-07-26 15:13:17 +0700

    Slices : ARRAYS IN GO

--- main.go
+++ main.go
@@ -1,97 +1,78 @@
-// Loops : CONTINUE
+// Slices : ARRAYS IN GO
 
 // ASSIGNMENT
-// As an easter egg, we decided to reward our users with a free text message if they send a prime number of text messages this year.
-// Complete the printPrimes function. It should print all of the prime numbers up to and including max. It should skip any numbers that are not prime.
+// When a message isn't responded to, we allow our clients to have up to 2 additional messages that are sent as nudging reminders.
 
-// Here's the psuedocode:
-//
-// printPrimes(max):
-//   for n in range(2, max+1):
-//     if n is 2:
-//       n is prime, print it
-//     if n is even:
-//       n is not prime, skip to next n
-//     for i in range (3, sqrt(n) + 1):
-//       if i can be multiplied into n:
-//         n is not prime, skip to next n
-//     n is prime, print it
+// getMessageWithRetries returns an array of 3 strings where index 0 is the first message. If the first message isn't answered by the recipient, we send the second, if that one isn't answered then we send the third.
 
-// BREAKDOWN
-// We skip even numbers because they can't be prime
-// We only check up to the square root because anything higher than the square root has no chance of multiplying evenly into n
-// We start checking at 2 because 1 is not prime
+// Update getMessageWithRetries to return the following 3 strings in an array.
+
+// click here to sign up
+// pretty please click here
+// we beg you to sign up
 
 package main
 
-import (
-	"fmt"
+import "fmt"
+
+const (
+	retry1 = "click here to sign up"
+	retry2 = "pretty please click here"
+	retry3 = "we beg you to sign up"
 )
 
-func printPrimes(max int) {
-	for n := 2; n < max+1; n++ {
-		if n == 2 {
-			fmt.Println(n)
-			continue
-		}
-		if n%2 == 0 {
-			continue
-		}
-		isPrime := true
-		for i := 3; i*i < n+i; i++ {
-			if n%i == 0 {
-				isPrime = false
-				break
-			}
-		}
-		if !isPrime {
-			continue
-		}
-		fmt.Println(n)
+func getMessageWithRetries() [3]string {
+	return [3]string{
+		"click here to sign up",
+		"pretty please click here",
+		"we beg you to sign up",
 	}
 }
 
-// don't edit below this line
+// don't touch below this line
 
-func test(max int) {
-	fmt.Printf("Primes up to %v:\n", max)
-	printPrimes(max)
-	fmt.Println("===============================================================")
+func testSend(name string, doneAt int) {
+	fmt.Printf("sending to %v...", name)
+	fmt.Println()
+
+	messages := getMessageWithRetries()
+	for i := 0; i < len(messages); i++ {
+		msg := messages[i]
+		fmt.Printf(`sending: "%v"`, msg)
+		fmt.Println()
+		if i == doneAt {
+			fmt.Println("they responded!")
+			break
+		}
+		if i == len(messages)-1 {
+			fmt.Println("complete failure")
+		}
+	}
 }
 
 func main() {
-	test(10)
-	test(20)
-	test(30)
+	testSend("Bob", 0)
+	testSend("Alice", 1)
+	testSend("Mangalam", 2)
+	testSend("Ozgur", 3)
 }
 
 // RESULTS:
 
-// Primes up to 10:
-// 2
-// 3
-// 5
-// 7
-// ===============================================================
-// Primes up to 20:
-// 2
-// 3
-// 5
-// 7
-// 11
-// 13
-// 17
-// 19
-// ===============================================================
-// Primes up to 30:
-// 2
-// 3
-// 5
-// 7
-// 11
-// 13
-// 17
-// 19
-// 23
-// 29
-// ===============================================================
+// sending to Bob...
+// sending: "click here to sign up"
+// they responded!
+// sending to Alice...
+// sending: "click here to sign up"
+// sending: "pretty please click here"
+// they responded!
+// sending to Mangalam...
+// sending: "click here to sign up"
+// sending: "pretty please click here"
+// sending: "we beg you to sign up"
+// they responded!
+// sending to Ozgur...
+// sending: "click here to sign up"
+// sending: "pretty please click here"
+// sending: "we beg you to sign up"
+// complete failure
