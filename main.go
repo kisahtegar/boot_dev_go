334580f3861641e528dcfb20adcbdb11a8249d61
kisah <data.kisahtegar@gmail.com>
2023-07-26 11:37:36 +0700

    Loops : LOOPS IN GO

--- main.go
+++ main.go
@@ -1,50 +1,50 @@
-// Errors : THE ERROR PACKAGE
+// Loops : LOOPS IN GO
 
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
-func divide(x, y float64) (float64, error) {
-	if y == 0 {
-		return 0.0, errors.New("No dividing by zero")
+func bulkSend(numMessages int) float64 {
+	totalCost := 0.0
+	for i := 0; i < numMessages; i++ {
+		totalCost += 1.0 + (0.01 * float64(i))
 	}
-	return x / y, nil
+	return totalCost
 }
 
 // don't edit below this line
 
-func test(x, y float64) {
-	defer fmt.Println("====================================")
-	fmt.Printf("Dividing %.2f by %.2f ...\n", x, y)
-	quotient, err := divide(x, y)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("Quotient: %.2f\n", quotient)
+func test(numMessages int) {
+	fmt.Printf("Sending %v messages\n", numMessages)
+	cost := bulkSend(numMessages)
+	fmt.Printf("Bulk send complete! Cost = %.2f\n", cost)
+	fmt.Println("===============================================================")
 }
 
 func main() {
-	test(10, 0)
-	test(10, 2)
-	test(15, 30)
-	test(6, 3)
+	test(10)
+	test(20)
+	test(30)
+	test(40)
+	test(50)
 }
 
 // RESULTS:
 
-// Dividing 10.00 by 0.00 ...
-// No dividing by zero
-// ====================================
-// Dividing 10.00 by 2.00 ...
-// Quotient: 5.00
-// ====================================
-// Dividing 15.00 by 30.00 ...
-// Quotient: 0.50
-// ====================================
-// Dividing 6.00 by 3.00 ...
-// Quotient: 2.00
-// ====================================
+// Sending 10 messages
+// Bulk send complete! Cost = 10.45
+// ===============================================================
+// Sending 20 messages
+// Bulk send complete! Cost = 21.90
+// ===============================================================
+// Sending 30 messages
+// Bulk send complete! Cost = 34.35
+// ===============================================================
+// Sending 40 messages
+// Bulk send complete! Cost = 47.80
+// ===============================================================
+// Sending 50 messages
+// Bulk send complete! Cost = 62.25
+// ===============================================================
