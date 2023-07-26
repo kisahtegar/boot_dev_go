e602f9a6207332838f2c73a6978596089c46c514
kisah <data.kisahtegar@gmail.com>
2023-07-26 21:30:23 +0700

    Pointer : NIL POINTERS

--- main.go
+++ main.go
@@ -1,43 +1,18 @@
 /*
  */
-// Pointer : INTRODUCTION TO POINTERS
+// Pointer : NIL POINTERS
 
-// POINTERS
-// Pointers hold the memory address of a value.
-/*
-	// The * syntax defines a pointer:
-	var p *int
-	A pointer's zero value is nil
+// NIL POINTERS
+// Pointers can be very dangerous.
 
-	// The & operator generates a pointer to its operand.
-	myString := "hello"
-	myStringPtr = &myString
+// If a pointer points to nothing (the zero value of the pointer type) then dereferencing
+// it will cause a runtime error (a panic) that crashes the program. Generally speaking,
+// whenever you're dealing with pointers you should check if it's nil before trying to
+// dereference it.
 
-	// The * dereferences a pointer to gain access to the value
-	fmt.Println(*myStringPtr) // read myString through the pointer
-	*myStringPtr = "world"    // set myString through the pointer
-*/
-// Unlike C, Go has no pointer arithmetic
-
-// [ JUST BECAUSE YOU CAN DOESN'T MEAN YOU SHOULD ]
-//
-// We're doing this exercise to understand that pointers can be used in this way.
-// That said, pointers can be very dangerous. It's generally a better idea to have
-// your functions accept non-pointers and return new values rather than mutating
-// pointer inputs.
-
-// [ ASSIGNMENT ]
-// Complete the removeProfanity function.
-
-// It should use the strings.ReplaceAll function to replace all instances of the
-// following words in the input message with asterisks.
-// - "dang" -> "****"
-// - "shoot" -> "*****"
-// - "heck" -> "****"
-
-// It should mutate the value in the pointer and return nothing.
-
-// Do not alter the function signature.
+// ASSIGNMENT
+// Let's make our profanity checker safe. Update the removeProfanity function. If message
+// is nil, return early to avoid a panic. After all, there are no bad words to remove.
 
 package main
 
@@ -47,6 +22,9 @@ import (
 )
 
 func removeProfanity(message *string) {
+	if message == nil {
+		return
+	}
 	messageVal := *message
 	messageVal = strings.ReplaceAll(messageVal, "dang", "****")
 	messageVal = strings.ReplaceAll(messageVal, "shoot", "*****")
@@ -58,33 +36,47 @@ func removeProfanity(message *string) {
 
 func test(messages []string) {
 	for _, message := range messages {
-		removeProfanity(&message)
-		fmt.Println(message)
+		if message == "" {
+			removeProfanity(nil)
+			fmt.Println("nil message detected")
+		} else {
+			removeProfanity(&message)
+			fmt.Println(message)
+		}
 	}
 }
 
 func main() {
-	messages1 := []string{
+	messages := []string{
 		"well shoot, this is awful",
+		"",
 		"dang robots",
 		"dang them to heck",
+		"",
 	}
 
 	messages2 := []string{
 		"well shoot",
+		"",
 		"Allan is going straight to heck",
 		"dang... that's a tough break",
+		"",
 	}
 
-	test(messages1)
+	test(messages)
 	test(messages2)
+
 }
 
 // RESULTS:
 
 // well *****, this is awful
+// nil message detected
 // **** robots
 // **** them to ****
+// nil message detected
 // well *****
+// nil message detected
 // Allan is going straight to ****
 // ****... that's a tough break
+// nil message detected
