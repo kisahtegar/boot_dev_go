4218c6afbccf76b4b288ee9273fd700f9babcd3e
kisah <data.kisahtegar@gmail.com>
2023-07-26 15:50:17 +0700

    Slices : VARIADIC

--- main.go
+++ main.go
@@ -1,157 +1,98 @@
 /*
  */
-// Slices : MAKE
+// Slices : VARIADIC
 
-// MAKE
-// Most of the time we don't need to think about the underlying array of a slice.
-// We can create a new slice using the make function:
+// VARIADIC
+// Many functions, especially those in the standard library, can take an arbitrary number of final arguments. This is accomplished by using the "..." syntax in the function signature.
+
+// A variadic function receives the variadic arguments as a slice.
+
 /*
-	// func make([]T, len, cap) []T
-	mySlice := make([]int, 5, 10)
-
-	// the capacity argument is usually omitted and defaults to the length
-	mySlice := make([]int, 5)
-	Slices created with make will be filled with the zero value of the type.
-
+	func concat(strs ...string) string {
+	    final := ""
+	    // strs is just a slice of strings
+	    for _, str := range strs {
+	        final += str
+	    }
+	    return final
+	}
 */
 
-// If we want to create a slice with a specific set of values, we can use a slice literal:
 /*
-	mySlice := []string{"I", "love", "go"}
-	Note that the array brackets do not have a 3 in them. If they did, you'd have an array instead of a slice.
+	func main() {
+	    final := concat("Hello ", "there ", "friend!")
+	    fmt.Println(final)
+	    // Output: Hello there friend!
+	}
 */
 
-// LENGTH
-// The length of a slice is simply the number of elements it contains. It is accessed using the built-in len() function:
+// The familiar fmt.Println() and fmt.Sprintf() are variadic! fmt.Println() prints each element with
+// space delimiters and a newline at the end.
 /*
-	mySlice := []string{"I", "love", "go"}
-	fmt.Println(len(mySlice)) // 3
+	func Println(a ...interface{}) (n int, err error)
 */
 
-// CAPACITY
-// The capacity of a slice is the number of elements in the underlying array, counting from the first element
-// in the slice. It is accessed using the built-in cap() function:
+// SPREAD OPERATOR
+// The spread operator allows us to pass a slice into a variadic function. The spread operator consists
+// of three dots following the slice in the function call.
 /*
-	mySlice := []string{"I", "love", "go"}
-	fmt.Println(cap(mySlice)) // 3
+	func printStrings(strings ...string) {
+		for i := 0; i < len(strings); i++ {
+			fmt.Println(strings[i])
+		}
+	}
+	func main() {
+	    names := []string{"bob", "sue", "alice"}
+	    printStrings(names...)
+	}
 */
-
-// Generally speaking, unless you're hyper-optimizing the memory usage of your program, you don't need to worry
-// about the capacity of a slice because it will automatically grow as needed.
 
 // ASSIGNMENT
-// We send a lot of text messages at Textio, and our API is getting slow and unresponsive.
+// We need to sum up the costs of all individual messages so we can send an end-of-month bill to our customers.
+// Complete the sum function so that returns the sum of all of its inputs.
 
-// If we know the rough size of a slice before we fill it up, we can make our program faster by creating the
-// slice with that size ahead of time so that the Go runtime doesn't need to continuously allocate new
-// underlying arrays of larger and larger sizes. By setting the length, the slice can still be resized later,
-// but it means we can avoid all the expensive resizing we know that we'll need.
-
-// Complete the getMessageCosts() function. It takes a slice of messages and returns a slice of message costs.
-
-// Preallocate a slice for the message costs of the same length as the messages slice.
-// Fill the costs slice with costs for each message. The cost in the cost slice should correspond to the
-// message in the messages slice at the same index. The cost of a message is the length of the message
-// multiplied by 0.01.
+// NOTE
+// Make note of how the variadic inputs and the spread operator are used in the test suite.
 
 package main
 
 import "fmt"
 
-func getMessageCosts(messages []string) []float64 {
-	costs := make([]float64, len(messages))
-	for i := 0; i < len(messages); i++ {
-		message := messages[i]
-		cost := float64(len(message)) * 0.01
-		costs[i] = cost
+func sum(nums ...float64) float64 {
+	total := 0.0
+	for i := 0; i < len(nums); i++ {
+		total += nums[i]
 	}
-	return costs
+	return total
 }
 
 // don't edit below this line
 
-func test(messages []string) {
-	costs := getMessageCosts(messages)
-	fmt.Println("Messages:")
-	for i := 0; i < len(messages); i++ {
-		fmt.Printf(" - %v\n", messages[i])
-	}
-	fmt.Println("Costs:")
-	for i := 0; i < len(costs); i++ {
-		fmt.Printf(" - %.2f\n", costs[i])
-	}
+func test(nums ...float64) {
+	total := sum(nums...)
+	fmt.Printf("Summing %v costs...\n", len(nums))
+	fmt.Printf("Bill for the month: %.2f\n", total)
 	fmt.Println("===== END REPORT =====")
 }
 
 func main() {
-	test([]string{
-		"Welcome to the movies!",
-		"Enjoy your popcorn!",
-		"Please don't talk during the movie!",
-	})
-	test([]string{
-		"I don't want to be here anymore",
-		"Can we go home?",
-		"I'm hungry",
-		"I'm bored",
-	})
-	test([]string{
-		"Hello",
-		"Hi",
-		"Hey",
-		"Hi there",
-		"Hey there",
-		"Hi there",
-		"Hello there",
-		"Hey there",
-		"Hello there",
-		"General Kenobi",
-	})
+	test(1.0, 2.0, 3.0)
+	test(1.0, 2.0, 3.0, 4.0, 5.0)
+	test(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0)
+	test(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0)
 }
 
 // RESULTS:
 
-// Messages:
-//  - Welcome to the movies!
-//  - Enjoy your popcorn!
-//  - Please don't talk during the movie!
-// Costs:
-//  - 0.22
-//  - 0.19
-//  - 0.35
+// Summing 3 costs...
+// Bill for the month: 6.00
 // ===== END REPORT =====
-// Messages:
-//  - I don't want to be here anymore
-//  - Can we go home?
-//  - I'm hungry
-//  - I'm bored
-// Costs:
-//  - 0.31
-//  - 0.15
-//  - 0.10
-//  - 0.09
+// Summing 5 costs...
+// Bill for the month: 15.00
 // ===== END REPORT =====
-// Messages:
-//  - Hello
-//  - Hi
-//  - Hey
-//  - Hi there
-//  - Hey there
-//  - Hi there
-//  - Hello there
-//  - Hey there
-//  - Hello there
-//  - General Kenobi
-// Costs:
-//  - 0.05
-//  - 0.02
-//  - 0.03
-//  - 0.03
-//  - 0.08
-//  - 0.09
-//  - 0.08
-//  - 0.11
-//  - 0.09
-//  - 0.11
-//  - 0.14
+// Summing 10 costs...
+// Bill for the month: 55.00
+// ===== END REPORT =====
+// Summing 15 costs...
+// Bill for the month: 120.00
 // ===== END REPORT =====
